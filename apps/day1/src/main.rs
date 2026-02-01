fa566f946719a052b664812e0556bddc1e730db3
Jacek Czupyt <kuba.czupyt@gmail.com>
2026-02-01 19:05:10 +0100

    Day 1.1

--- apps/day1/src/main.rs
+++ apps/day1/src/main.rs
@@ -1,13 +1,46 @@
+use std::fs;
 use common::launcher;
 
 fn main() {
     launcher::launch(part1, part2);
 }
 
-fn part1(_input: std::path::PathBuf) {
-    todo!()
+fn part1(input_path: std::path::PathBuf) {
+    let input = fs::read_to_string(input_path).unwrap();
+
+    let turns = prase_input(input);
+
+    let mut val = 50;
+    let mut count = 0;
+
+    for turn in turns {
+        val = (val + turn)%100;
+        if val == 0 {
+            count += 1;
+        }
+    }
+
+    println!("{}", count);
 }
 
 fn part2(_input: std::path::PathBuf) {
     todo!()
+}
+
+fn prase_input(input: String) -> Vec<i32> {
+    input
+        .lines()
+        .map(
+            |s| {
+                let dir = s.chars().next().unwrap();
+                let steps: i32 = s[1..].parse().unwrap();
+
+                match dir {
+                    'R' => steps,
+                    'L' => -steps,
+                    _ => panic!("Invalid direction"),
+                }
+            }
+        )
+        .collect()
 }
